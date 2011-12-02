4b2973aef7aea91d63f9b1ac16d8632d444b359f
royganor <roy@zend.com>
2011-12-02 23:53:32 +0200

    add chrome extension

--- test/zend-webapi-test.js
+++
@@ -1,6 +0,0 @@
-var applicationInfo = webApiClient.applicationDeploy("my content", "test");
-applicationInfo = webApiClient.applicationGetStatus(info.id);
-
-while (applicationInfo.status != 'OK') {
-	applicationInfo = webApiClient.applicationGetStatus(info.id);
-}
